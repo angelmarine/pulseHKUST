b45c5028757e5ee950b1eddf44f6f53c5692804b
felycia <fgunawan@ust.hk>
2018-01-30 18:04:28 +0700

    Add DB connection options & disconnect on termination

--- src/utils/initDbConnection.js
+++ src/utils/initDbConnection.js
@@ -2,10 +2,7 @@ var mongoose = require('mongoose');
 var logger = require('./logger');
 
 var option = {
-    server: {
-        poolSize: 10,
-        connectTimeoutMS: 30000
-    }
+    useMongoClient: true
 };
 
 // Set connection URL
@@ -13,10 +10,18 @@ var url = 'mongodb://127.0.0.1:27017/pulse-test';
 
 // Connect to the database
 mongoose.connect(url, option);
+mongoose.Promise = global.Promise;
 var db = mongoose.connection;
-db.on('error', err => logger.error(`Connection error: ${err.message}`));
+db.on('error', err => logger.error(`DB connection error: ${err.message}`));
 db.once('open', function(){
-        logger.info("Connection initialized successfully!");
-    });
+    logger.info("DB connection initialized successfully!");
+});
 
-module.exports = db;
+process.on('SIGINT', function(){
+    db.close(function(){
+        logger.info("DB connection disconnected due to application termination");
+        process.exit(0);
+    });
+});
+
+module.exports = mongoose.connection;
