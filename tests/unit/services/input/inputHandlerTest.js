13a8202a9072694f9e345c51576b991ef3f14a65
felycia <fgunawan@ust.hk>
2018-02-24 18:42:00 +0800

    Use moment.js for parsing date

--- tests/unit/services/input/inputHandlerTest.js
+++ tests/unit/services/input/inputHandlerTest.js
@@ -4,6 +4,7 @@
 const fs = require('fs');
 const should = require('chai').should();
 const sinon = require('sinon');
+const moment = require('moment');
 
 describe('inputHandler', function() {
     const lineParserResponse = {
@@ -16,42 +17,51 @@ describe('inputHandler', function() {
     const lineParser = sinon.stub();
     const storeInputData = sinon.stub();
 
+    // Program the response of external dependencies
+    lineParser.returns(lineParserResponse);
+    storeInputData.returns(Promise.resolve());
+
     const inputHandler = require('../../../../src/services/input/inputHandler')(lineParser, storeInputData);
 
     const directory = '/Users/felyciagunawan/dev/fyp/workspace/pulseHKUST/tests/unit/services/input/';
     const filename = 'filter_user_detail_20171113_1620';
 
-    it('should throw error if file not found', function() {
-        const missingFilename = 'filter_user_detail_20171213_1620';
+    const filePath = directory + filename;
+    const data = '000af58da724, 10.89.82.199, t602all07g, eduroam,\n' +
+        '000af5bfb060, 10.89.168.145, t700norbri01, eduroam,';
+    // Create input file with content
+    fs.writeFileSync(filePath, data);
 
-        const result = () => inputHandler(directory, missingFilename);
-        result.should.throw("File not found");
-    });
+    inputHandler(directory, filename);
 
-    it('should throw error on invalid filename', function() {
-        // Require creating a temporary file with invalid filename
-        const invalidFilename = 'filter_user_detail_2017113_1620';
-        const invalidFilePath = directory + invalidFilename;
-        fs.writeFileSync(invalidFilePath, "");
-
-        const result = () => inputHandler(directory, invalidFilename);
-        result.should.throw("Invalid filename");
-
-        fs.unlink(invalidFilePath);
-    });
-
-    it('should parse input line by line and add the correct timestamp', function() {
-        const expectedTimestamp = new Date(2017, 11, 13, 16, 20);
-
-        // Program the response of external dependencies
-        lineParser.returns(lineParserResponse);
-        storeInputData.returns(Promise.resolve());
-
-        inputHandler(directory, filename);
+    it('should parse input line by line', function() {
         lineParser.calledTwice.should.equal(true);
+    });
 
+    it('should add the correct timestamp', function() {
+        // Javascript date represents months as [0-11]
+        const expectedTimestamp = new Date(2017, 10, 13, 16, 20);
         const argsTimestamp = (((storeInputData.args[0])[0])[0]).Timestamp.getTime();
         argsTimestamp.should.equal(expectedTimestamp.getTime());
     });
+
+    it('should delete the input file', function() {
+        const fileExists = fs.existsSync(filePath);
+        fileExists.should.equal(false);
+    });
+
+
+    //TODO: function moved to router
+    // it('should throw error on invalid filename', function() {
+    //     // Require creating a temporary file with invalid filename
+    //     const invalidFilename = 'filter_user_detail_2017113_1620';
+    //     const invalidFilePath = directory + invalidFilename;
+    //     fs.writeFileSync(invalidFilePath, "");
+    //
+    //     const result = () => inputHandler(directory, invalidFilename);
+    //     result.should.throw("Invalid filename");
+    //
+    //     fs.unlink(invalidFilePath);
+    // });
 
 });
