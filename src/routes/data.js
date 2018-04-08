2054eff46c906da32c8b8e16cc8576a289b3dee8
felycia <fgunawan@ust.hk>
2018-04-08 21:58:26 +0800

    Changes for data API

--- src/routes/data.js
+++ src/routes/data.js
@@ -8,7 +8,8 @@ const isValidDate = date => moment(date, 'YYYY-MM-DD', true).isValid();
 
 router.get('/date/:date', function(req, res, next) {
     if(isValidDate(req.params.date)) {
-        locationDataServices.getDataForDate(req.params.date)
+        const date = moment(req.params.date, 'YYYY-MM-DD');
+        locationDataServices.getDataForDate(date)
             .then(data => res.send({data}));
     } else {
         res.status(400).send({'message': 'Date must be specified in YYYY-MM-DD format.'});
@@ -17,8 +18,29 @@ router.get('/date/:date', function(req, res, next) {
 
 router.get('/range/from=:from&to=:to', function(req, res, next) {
     if(isValidDate(req.params.from) && isValidDate(req.params.to)) {
-        locationDataServices.getDataForDateRange(req.params.from, req.params.to)
-            .then(data => res.send({data}));
+        const startDate = moment(req.params.from, 'YYYY-MM-DD');
+        const endDate = moment(req.params.to, 'YYYY-MM-DD');
+        if(startDate.isSameOrBefore(endDate)) {
+            locationDataServices.getDataForDateRange(startDate, endDate)
+                .then(data => res.send({data}));
+        } else {
+            res.status(400).send({'message': 'Start date must be same or before end date.'})
+        }
+    } else {
+        res.status(400).send({'message': 'Dates must be specified in YYYY-MM-DD format.'})
+    }
+});
+
+router.get('/stream/from=:from&to=:to', function(req, res, next) {
+    if(isValidDate(req.params.from) && isValidDate(req.params.to)) {
+        const startDate = moment(req.params.from, 'YYYY-MM-DD');
+        const endDate = moment(req.params.to, 'YYYY-MM-DD');
+        if(startDate.isSameOrBefore(endDate)) {
+            locationDataServices.getStreamGraphData(startDate, endDate)
+                .then(data => res.send({data}));
+        } else {
+            res.status(400).send({'message': 'Start date must be same or before end date.'})
+        }
     } else {
         res.status(400).send({'message': 'Dates must be specified in YYYY-MM-DD format.'})
     }
--- src/services/locationDataServices.js
+++ src/services/locationDataServices.js
@@ -1,22 +1,57 @@
 const moment = require('moment');
+const R = require('ramda');
 
 const make = (locationDataRepo = require('../database/utils/locationDataRepo')(),
                 apGroupHandler = require('./input/apGroupHandler')) => {
 
-    const getDataForDate = dateString => {
-        const date = moment(dateString, 'YYYY-MM-DD');
+    const getDataForDate = date => {
         const apGroupList = apGroupHandler.getApGroupList();
         return locationDataRepo.findByApIdsAndDay(apGroupList, date);
     };
 
-    const getDataForDateRange = (startDateStr, endDateStr) => {
-        const startDate = moment(startDateStr, 'YYYY-MM-DD');
-        const endDate = moment(endDateStr, 'YYYY-MM-DD');
-        const apGroupList = apGroupHandler.getApGroupList();
+    const getDataForDateRange = (startDate, endDate, apGroupList = apGroupHandler.getApGroupList()) => {
         return locationDataRepo.findByApIdsAndRange(apGroupList, startDate, endDate);
     };
 
-    return {getDataForDate, getDataForDateRange}
+    const getStreamGraphData = (startDate, endDate) => {
+        const norm = endDate.diff(startDate, 'days') + 1;
+        const newData = [];
+        const mapData = (datum) => {
+            const frequency = [];
+            const mapDay = (obj) => {
+                const date = moment(obj['Day']);
+                const mapHour = (val, key, obj) => {
+                    if(key === "_id") return;
+                    const hour = key;
+                    const mapMinute = (val2, key2, obj) => {
+                        if(key2 === "_id") return;
+                        const completeDate = date.clone().set('hour', hour).set('minute', key2);
+                        frequency.push({x: completeDate, y: val2});
+                    };
+                    R.mapObjIndexed(mapMinute, val);
+                };
+                R.mapObjIndexed(mapHour, obj['Hour_minute_count'])
+            };
+            R.map(mapDay, datum['Count_timestamp']);
+            if(norm < 2) {
+                newData.push({theme: datum['AP_id'], frequency: frequency});
+            } else {
+                const filteredFreq = [];
+                for (let i=0; i<frequency.length; i += norm) {
+                    filteredFreq.push(frequency[i]);
+                }
+                newData.push({theme: datum['AP_id'], frequency: filteredFreq});
+            }
+        };
+
+        return getDataForDateRange(startDate, endDate, apGroupHandler.getCompactList())
+            .then(data => {
+                R.map(mapData, data);
+                return newData;
+            })
+    };
+
+    return {getDataForDate, getDataForDateRange, getStreamGraphData}
 };
 
 module.exports = make;
