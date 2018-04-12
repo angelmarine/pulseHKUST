b93ee27235c48bece2d53da7a5ce05f0e28862d1
felycia <fgunawan@ust.hk>
2018-04-12 11:49:15 +0800

    Do not use upsert to increase performance & add query for getting hourly avg dwelling time

--- src/database/utils/dwellTimeDataRepo.js
+++ src/database/utils/dwellTimeDataRepo.js
@@ -4,17 +4,38 @@ const R = require('ramda');
 const insertMany = data => {
     const createUpdateReq = (datum) => {
         return {
-            updateOne: {
-                filter: {MAC_id: datum['MAC_id'], Start_ts: datum['Start_ts']},
-                update: {
-                    $set: {AP_group: datum['AP_group'], End_ts: datum['End_ts'], Duration: datum['Duration']}
-                },
-                upsert: true
+            insertOne: {
+                document: {
+                    MAC_id: datum['MAC_id'],
+                    AP_group: datum['AP_group'],
+                    Start_ts: datum['Start_ts'],
+                    End_ts: datum['End_ts'],
+                    Duration: datum['Duration']
+                }
             }
         };
     };
-
     return dwellTimeDataModel.bulkWrite(R.map(createUpdateReq, data))
 };
 
-module.exports = {insertMany};
+const getHourlyAvgForDay = (AP_group, date) => {
+    const startDate = date.startOf('day').toDate();
+    const endDate = date.endOf('day').toDate();
+
+    return dwellTimeDataModel.aggregate([
+        {
+            $match: {
+                'AP_group': AP_group,
+                'Start_ts': {$gte: startDate, $lte: endDate}
+            }
+        },
+        {
+            $group: {_id: {$hour: '$Start_ts'}, avg: {$avg: '$Duration'}}
+        },
+        {
+            $project: {'hour': '$_id', avg:1, _id:0}
+        }
+    ])
+};
+
+module.exports = {insertMany, getHourlyAvgForDay};
